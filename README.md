58fa8a3172c6937f2c6bbc126c78928d5a47cf5c
Marcio Frayze David <marcio.david@serpro.gov.br>
2019-09-10 15:28:09 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Lambda Racing
+
+This will be a simple old school racing game written in [haskell](https://www.haskell.org/) using the
+[gloss](http://hackage.haskell.org/package/gloss) library.
+
+Right now this is just a test and no actual game is available yet.
---
+++ src/Main.hs
@@ -0,0 +1,67 @@
+module Main(main) where
+
+import Graphics.Gloss
+import Graphics.Gloss.Data.ViewPort
+import Graphics.Gloss.Interface.IO.Interact
+
+width, height, offset :: Int
+width = 800
+height = 600
+offset = 100
+fps = 60
+
+playerSpeed = 4.0
+
+carWidth = 20
+carLength = 30
+
+window :: Display
+window = InWindow "Lambda Racing 2019" (width, height) (offset, offset)
+
+background :: Color
+background = black
+
+main :: IO ()
+main = play window background fps initialState draw handleKeys update
+
+update :: Float -> LambdaRacing -> LambdaRacing
+update seconds game = (movePlayers) game
+
+type Position = (Float, Float)
+data PlayerMovement = PlayerLeft | PlayerStill | PlayerRight deriving Show
+
+data LambdaRacing = Game
+  { player :: Position
+  , playerMovement :: PlayerMovement
+  } deriving Show
+
+initialState :: LambdaRacing
+initialState = Game
+  (120.0, 100.0) PlayerStill
+
+draw :: LambdaRacing -> Picture
+draw game = pictures [mkCar (player game)]
+  where
+
+    mkCar :: Position -> Picture
+    mkCar pos = pictures
+      [ translate x y $ color orange $ rectangleSolid (2 * carWidth) (2 * carLength)
+      ]
+      where (x, y) = pos
+
+movePlayer :: Position -> PlayerMovement -> Position
+movePlayer (px, py) PlayerLeft = (px - playerSpeed, py)
+movePlayer (px, py) PlayerStill = (px, py)
+movePlayer (px, py) PlayerRight = (px + playerSpeed, py)
+
+movePlayers :: LambdaRacing -> LambdaRacing
+movePlayers game = game {  player = movePlayer (player game) (playerMovement game)
+                        }
+
+handleKeys :: Event -> LambdaRacing -> LambdaRacing
+handleKeys (EventKey (SpecialKey KeyLeft) Down _ _) game = game { playerMovement = PlayerLeft }
+--handleKeys (EventKey (SpecialKey KeyLeft) Up _ _) game = game { playerMovement = PlayerStill }
+handleKeys (EventKey (SpecialKey KeyRight) Down _ _) game = game { playerMovement = PlayerRight }
+--handleKeys (EventKey (SpecialKey KeyRight) Up _ _) game = game { playerMovement = PlayerStill }
+
+handleKeys _ game = game
